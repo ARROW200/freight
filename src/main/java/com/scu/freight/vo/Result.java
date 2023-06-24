4491317d3584fee8335fdf6510cb5c55c0cf14a5
hlw <2024220656@qq.com>
2023-06-24 15:43:18 +0800

    Initial commit

---
+++ src/main/java/com/scu/freight/vo/Result.java
@@ -0,0 +1,77 @@
+package com.scu.freight.vo;
+
+/**
+ * @author hazel
+ * @description 功能描述
+ * @create 2023/6/24 15:26
+ */
+public class Result<T> {
+    private Integer code;
+    private String message;
+    private T data;
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public Result(Integer code, String message, T data) {
+        this.code = code;
+        this.message = message;
+        this.data = data;
+    }
+
+    public Result() {
+    }
+
+    public static <T> Result<T> success(){
+        return new Result<>(20000,"success",null);
+    }
+
+    public static <T> Result<T> success(T data){
+        return new Result<>(20000,"success",data);
+    }
+
+    public static <T> Result<T> success(T data, String message){
+        return new Result<>(20000,message,data);
+    }
+
+    public static <T> Result<T> success(String message){
+        return new Result<>(20000,message,null);
+    }
+
+    public static<T>  Result<T> fail(){
+        return new Result<>(20001,"fail",null);
+    }
+
+    public static<T>  Result<T> fail(Integer code){
+        return new Result<>(code,"fail",null);
+    }
+
+    public static<T>  Result<T> fail(Integer code, String message){
+        return new Result<>(code,message,null);
+    }
+
+    public static<T>  Result<T> fail( String message){
+        return new Result<>(20001,message,null);
+    }
+}
